ae12e0cebaf21e2185287ee78efe78363716206b
Davide Petrillo <davide@ustwo.co.uk>
2013-12-12 15:43:17 +0000

    Fixed touchend events not being removed when $.off is called

--- src/touche.js
+++ src/touche.js
@@ -59,16 +59,31 @@
 
   // Has the developer used jQuery?
   if (window.jQuery && isTouch) {
-    var originalOnMethod = jQuery.fn.on;
+    var originalOnMethod  = jQuery.fn.on,
+        originalOffMethod = jQuery.fn.off;
+
+    var replaceEventName = function (event) {
+      if (event.slice(0, 5) == 'click') {
+        return event.replace('click', 'touchend');
+      }
+      return event;
+    }
 
     // Change event type and re-apply .on() method
     jQuery.fn.on = function() {
-      var event = arguments[0];
-      
-      if( event.slice(0, 5) == 'click' )
-        arguments[0] = event.replace('click', 'touchend');
+      // arguments[0] is the event name
+      arguments[0] = replaceEventName(arguments[0]);
         
       originalOnMethod.apply(this, arguments);
+      return this;
+    };
+
+    // Change event type and re-apply .off() method
+    jQuery.fn.off = function() {
+      // arguments[0] is the event name
+      arguments[0] = replaceEventName(arguments[0]);
+        
+      originalOffMethod.apply(this, arguments);
       return this;
     };
   }
